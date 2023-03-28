b5883bc636abdf20fc21cda52b368bfe8f83986d
Timothy Lee <106719865+timooo-thy@users.noreply.github.com>
2023-03-29 00:25:21 +0800

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-function updateResetDate() {
-			const dateInput = document.getElementById("date");
-			const resetDateInput = document.getElementById("reset-date");
-			resetDateInput.value = dateInput.value;
-		}
-
-function displayMessage(message) {
-    alert(message);
-}
